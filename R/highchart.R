2d35558ba5557ca3a70eff1f2da6350a4616e78c
Joshua <jbkunst@gmail.com>
2015-12-11 13:39:10 -0300

    skeleton package

---
+++ R/highchart.R
@@ -0,0 +1,38 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+highchart <- function(message, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'highchart',
+    x,
+    width = width,
+    height = height,
+    package = 'highcharter'
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @export
+highchartOutput <- function(outputId, width = '100%', height = '400px'){
+  shinyWidgetOutput(outputId, 'highchart', width, height, package = 'highcharter')
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @export
+renderHighchart <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  shinyRenderWidget(expr, highchartOutput, env, quoted = TRUE)
+}
