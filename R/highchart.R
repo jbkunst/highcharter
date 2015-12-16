fb20e7359d9485787c1eccc6e0757680c3bd4d95
Joshua <jbkunst@gmail.com>
2015-12-16 15:10:56 -0300

    completing the API and crating index.htmlñ

--- R/highchart.R
+++ R/highchart.R
@@ -2,16 +2,63 @@
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#chart}
 #' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
 #' @export
+
 hc_chart <- function(hc, ...) {
   hc$x$hc_opts$chart <- list(...)
   hc
 }
 
+#' Adding credits options to highchart object
+#'
+#' Arguments are defined in \url{http://api.highcharts.com/highcharts#credits}
+#' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
+#' @export
+hc_credits <- function(hc, ...) {
+  hc$x$hc_opts$credits <- list(...)
+  hc
+}
+
+#' Adding exporting options to highchart object
+#'
+#' Arguments are defined in \url{http://api.highcharts.com/highcharts#exporting}
+#' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
+#' @export
+hc_exporting <- function(hc, ...) {
+  hc$x$hc_opts$exporting <- list(...)
+  hc
+}
+
+#' Adding legend options to highchart object
+#'
+#' Arguments are defined in \url{http://api.highcharts.com/highcharts#legend}
+#' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
+#' @export
+hc_legend <- function(hc, ...) {
+  hc$x$hc_opts$legend <- list(...)
+  hc
+}
+
+
 #' Adding titles options to highchart object
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#title}
 #' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
 #' @export
 hc_title <- function(hc, ...) {
   hc$x$hc_opts$title <- list(...)
@@ -22,9 +69,25 @@ hc_title <- function(hc, ...) {
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#subtitle}
 #' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
 #' @export
 hc_subtitle <- function(hc, ...) {
   hc$x$hc_opts$subtitle <- list(...)
+  hc
+}
+
+#' Adding tooltip options to highchart object
+#'
+#' Arguments are defined in \url{http://api.highcharts.com/highcharts#tooltip}
+#' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
+#' @export
+hc_tooltip <- function(hc, ...) {
+  hc$x$hc_opts$tooltip <- list(...)
   hc
 }
 
@@ -32,6 +95,9 @@ hc_subtitle <- function(hc, ...) {
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#xAxis}
 #' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
 #' @export
 hc_xAxis  <- function(hc, ...) {
   hc$x$hc_opts$xAxis <- list(...)
@@ -42,6 +108,9 @@ hc_xAxis  <- function(hc, ...) {
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#yAxis}
 #' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
 #' @export
 hc_yAxis  <- function(hc, ...) {
   hc$x$hc_opts$yAxis <- list(...)
@@ -52,17 +121,22 @@ hc_yAxis  <- function(hc, ...) {
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#plotOptions}
 #' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
 #' @export
 hc_plotOptions  <- function(hc, ...) {
   hc$x$hc_opts$plotOptions <- list(...)
   hc
 }
 
-
 #' Adding series to highchart object
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#chart}
 #' 
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param ... Arguments defided in \url{http://api.highcharts.com/highcharts}. 
+#'
 #' @export
 hc_add_serie <- function(hc, ...) {
   hc$x$hc_opts$series <- append(hc$x$hc_opts$series, list(list(...)))
@@ -73,6 +147,9 @@ hc_add_serie <- function(hc, ...) {
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#chart}
 #'
+#' @param hc A \code{highchart} \code{htmlwidget} object. 
+#' @param name The serie's name to delete.
+#'
 #' @import purrr
 #' 
 #' @export
@@ -80,10 +157,10 @@ hc_rm_serie <- function(hc, name = NULL) {
   
   stopifnot(!is.null(name))
   
-  position <- hc$x$hc_opts$series %>%
+  positions <- hc$x$hc_opts$series %>%
     map("name") %>%
-    unlist() %>% 
-    {which(. == name)}
+    unlist()
+  position <- which(positions == name)
   
   hc$x$hc_opts$series[position] <- NULL
   
@@ -94,6 +171,12 @@ hc_rm_serie <- function(hc, name = NULL) {
 #'
 #' <Add Description>
 #'
+#' @param hc_opts A options defined as \url{http://api.highcharts.com/highcharts}.
+#' @param width A numeric input in pixels.
+#' @param height  A numeric input in pixels.
+#' @param debug A boolean value if you want to print in the browser console the 
+#'    parameters given to \code{highchart}.
+#'      
 #' @import htmlwidgets
 #'
 #' @export
@@ -117,12 +200,20 @@ highchart <- function(hc_opts = list(), width = NULL, height = NULL, debug = FAL
 
 #' Widget output function for use in Shiny
 #'
+#' @param outputId The name of the input.
+#' @param width A numeric input in pixels.
+#' @param height  A numeric input in pixels. 
+#'
 #' @export
 highchartOutput <- function(outputId, width = '100%', height = '400px'){
   shinyWidgetOutput(outputId, 'highchart', width, height, package = 'highcharter')
 }
 
 #' Widget render function for use in Shiny
+#'
+#' @param expr A highchart expression. 
+#' @param env A enviorment.
+#' @param quoted  A boolean value.
 #'
 #' @export
 renderHighchart <- function(expr, env = parent.frame(), quoted = FALSE) {
