7bbf9f9a1c01b5ed2a92453fab285d0983ec6146
Joshua <jbkunst@gmail.com>
2015-12-18 11:36:36 -0300

    ordering R folder and  trying make work export! :sad:

--- R/highchart-api.R
+++ R/highchart-api.R
@@ -42,20 +42,6 @@ hc_credits <- function(hc, ...) {
   
 }
 
-#' Adding exporting options to highchart object
-#'
-#' Arguments are defined in \url{http://api.highcharts.com/highcharts#exporting}
-#' 
-#' @param hc A \code{highchart} \code{htmlwidget} object. 
-#' @param ... Arguments defined in \url{http://api.highcharts.com/highcharts}. 
-#'
-#' @export
-hc_exporting <- function(hc, ...) {
-  
-  .hc_opt(hc, "exporting", ...)
-  
-}
-
 #' Adding legend options to highchart object
 #'
 #' Arguments are defined in \url{http://api.highcharts.com/highcharts#legend}
@@ -194,65 +180,4 @@ hc_rm_serie <- function(hc, name = NULL) {
   
   hc
   
-}
-
-#' Create a Highcharts chart widget
-#'
-#' This function creates a Highchart chart using \pkg{htmlwidgets}. The
-#' widget can be rendered on HTML pages generated from R Markdown, Shiny, or
-#' other applications.
-#'
-#' @param hc_opts A \code{list} object containing options defined as 
-#'    \url{http://api.highcharts.com/highcharts}.
-#' @param width A numeric input in pixels.
-#' @param height  A numeric input in pixels.
-#' @param debug A boolean value if you want to print in the browser console the 
-#'    parameters given to \code{highchart}.
-#'      
-#' @import htmlwidgets
-#'
-#' @export
-highchart <- function(hc_opts = list(), width = NULL,
-                      height = NULL, debug = FALSE) {
-
-  # forward options using x
-  x <- list(
-    hc_opts = hc_opts,
-    debug = debug
-  )
-
-  # create widget
-  htmlwidgets::createWidget(
-    name = "highchart",
-    x,
-    width = width,
-    height = height,
-    package = "highcharter"
-  )
-}
-
-#' Widget output function for use in Shiny
-#'
-#' @param outputId The name of the input.
-#' @param width A numeric input in pixels.
-#' @param height  A numeric input in pixels. 
-#'
-#' @export
-highchartOutput <- function(outputId, width = "100%", height = "400px"){
-  shinyWidgetOutput(outputId, "highchart", width, height,
-                    package = "highcharter")
-}
-
-#' Widget render function for use in Shiny
-#'
-#' @param expr A highchart expression. 
-#' @param env A enviorment.
-#' @param quoted  A boolean value.
-#'
-#' @export
-renderHighchart <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) {
-    expr <- substitute(expr)
-    } # force quoted
-  shinyRenderWidget(expr, highchartOutput, env, quoted = TRUE)
 }
---
+++ R/highchart-htmlwidget.R
@@ -0,0 +1,61 @@
+
+#' Create a Highcharts chart widget
+#'
+#' This function creates a Highchart chart using \pkg{htmlwidgets}. The
+#' widget can be rendered on HTML pages generated from R Markdown, Shiny, or
+#' other applications.
+#'
+#' @param hc_opts A \code{list} object containing options defined as 
+#'    \url{http://api.highcharts.com/highcharts}.
+#' @param width A numeric input in pixels.
+#' @param height  A numeric input in pixels.
+#' @param debug A boolean value if you want to print in the browser console the 
+#'    parameters given to \code{highchart}.
+#'      
+#' @import htmlwidgets
+#'
+#' @export
+highchart <- function(hc_opts = list(), width = NULL,
+                      height = NULL, debug = FALSE) {
+
+  # forward options using x
+  x <- list(
+    hc_opts = hc_opts,
+    debug = debug
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = "highchart",
+    x,
+    width = width,
+    height = height,
+    package = "highcharter"
+  )
+}
+
+#' Widget output function for use in Shiny
+#'
+#' @param outputId The name of the input.
+#' @param width A numeric input in pixels.
+#' @param height  A numeric input in pixels. 
+#'
+#' @export
+highchartOutput <- function(outputId, width = "100%", height = "400px"){
+  shinyWidgetOutput(outputId, "highchart", width, height,
+                    package = "highcharter")
+}
+
+#' Widget render function for use in Shiny
+#'
+#' @param expr A highchart expression. 
+#' @param env A enviorment.
+#' @param quoted  A boolean value.
+#'
+#' @export
+renderHighchart <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) {
+    expr <- substitute(expr)
+    } # force quoted
+  shinyRenderWidget(expr, highchartOutput, env, quoted = TRUE)
+}
