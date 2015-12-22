80ffc6722f6dddf42a1d4f174f42b7dd168e5f3f
Joshua <jbkunst@gmail.com>
2015-12-22 18:39:44 -0300

    testings

--- tests/testthat/test-highchart.R
+++ tests/testthat/test-highchart.R
@@ -7,3 +7,18 @@ test_that("highchart object is a from highchart and htmlwidget class", {
   expect_true(all(class(hc) %in% c("highchart",  "htmlwidget")))
   
 })
+
+test_that("highchartOutput  return a function", {
+  
+  expect_true(all(class(highchartOutput("outputid")) %in% c("shiny.tag.list", "list")))
+  
+})
+
+
+test_that("renderHighchart return a function", {
+  
+  expect_true(is.function(renderHighchart(highchart())))
+  
+})
+
+
