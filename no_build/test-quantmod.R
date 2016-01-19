12dcf5801cbf8d4ca150ab8f2871f14f848e7cd6
Joshua <jbkunst@gmail.com>
2016-01-19 16:33:33 -0300

    updating quantmod test

--- no_build/test-quantmod.R
+++ no_build/test-quantmod.R
@@ -1,4 +1,29 @@
-library('quantmod')
+rm(list = ls())
+require("quantmod")
 
-data <- getSymbols("SPY", src = "google", auto.assign = FALSE)
+getSymbols("SPY", src = "google")
+
+time <- time(SPY) %>% 
+  zoo::as.Date() %>% 
+  as.POSIXct() %>% 
+  as.numeric() %>% 
+  { 1000 * . }
+
+spy <- SPY[,-5] %>% 
+  as.data.frame() %>% 
+  {cbind(time, .)} %>% 
+  list.parse2()
+
+highchart(highstock = TRUE) %>% 
+  hc_add_series(type = "candlestick",
+                data = spy,
+                name = "SPY",
+                dataGrouping = list(
+                  units = list(
+                    list("week", 1),
+                    list("month", c(1,2,3,4,6))
+                  )
+                )
+  )
+
 
