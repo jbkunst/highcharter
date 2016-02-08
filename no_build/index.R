1160dc5a9e5e7d7d5bc134076e4c5c05e80751f5
Joshua <jbkunst@gmail.com>
2016-02-08 02:03:48 -0300

    fix in index

--- no_build/index.R
+++ no_build/index.R
@@ -566,46 +566,47 @@ highchart() %>%
   hc_add_series_map(worldgeojson, GNI2010,
                     value = "GNI", joinBy = "iso3")
 
-#' 
-#' Other fun example. Load various geojson and chart them all.
-#' 
-library("httr")
+#+ echo=FALSE, eval=FALSE
+# Other fun example. Load various geojson and chart them all.
+ 
+# library("httr")
+# 
+# world <- "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json" %>% 
+#   GET() %>% 
+#   content() %>% 
+#   jsonlite::fromJSON(simplifyVector = FALSE)
+# 
+# # http://cedeusdata.geosteiniger.cl/layers/geonode:mundo_corrientes_maritimas
+# marine <- "http://cedeusdata.geosteiniger.cl/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Amundo_corrientes_maritimas&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature" %>% 
+#   GET() %>% 
+#   content()
+# 
+# # http://cedeusdata.geosteiniger.cl/layers/geonode:mundo_limites_placas
+# plates <- "http://cedeusdata.geosteiniger.cl/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Amundo_limites_placas&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature" %>% 
+#   GET() %>% 
+#   content()
+# 
+# # http://cedeusdata.geosteiniger.cl/layers/geonode:mundo_volcanes
+# volcano <- "http://cedeusdata.geosteiniger.cl/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Amundo_volcanes&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature" %>% 
+#   GET() %>% 
+#   content()
+# 
+# highchart(type = "map") %>% 
+#   hc_add_series(mapData = world, showInLegend = FALSE) %>% 
+#   hc_add_series(data = marine, type = "mapline",  lineWidth = 2,
+#                 name = "Marine currents", color = 'rgba(0, 0, 80, 0.5)',
+#                 states = list(hover = list(color = "#BADA55")),
+#                 tooltip = list(pointFormat = "{point.properties.NOMBRE}")) %>%
+#   hc_add_series(data = plates, type = "mapline",
+#                 name = "Plates", color = 'rgba(10, 10, 10, 0.5)',
+#                 tooltip = list(pointFormat = "{point.properties.TIPO}")) %>% 
+#   hc_add_series(data = volcano, type = "mappoint",
+#                 name = "Volcanos", color = 'rgba(255, 0, 80, 0.5)',
+#                 tooltip = list(pointFormat = "{point.properties.NOMBRE}")) %>%
+#   hc_mapNavigation(enabled = TRUE) %>% 
+#   hc_title(text = "Testing geojson format")
 
-world <- "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json" %>% 
-  GET() %>% 
-  content() %>% 
-  jsonlite::fromJSON(simplifyVector = FALSE)
-
-# http://cedeusdata.geosteiniger.cl/layers/geonode:mundo_corrientes_maritimas
-marine <- "http://cedeusdata.geosteiniger.cl/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Amundo_corrientes_maritimas&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature" %>% 
-  GET() %>% 
-  content()
-
-# http://cedeusdata.geosteiniger.cl/layers/geonode:mundo_limites_placas
-plates <- "http://cedeusdata.geosteiniger.cl/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Amundo_limites_placas&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature" %>% 
-  GET() %>% 
-  content()
-
-# http://cedeusdata.geosteiniger.cl/layers/geonode:mundo_volcanes
-volcano <- "http://cedeusdata.geosteiniger.cl/geoserver/wfs?srsName=EPSG%3A4326&typename=geonode%3Amundo_volcanes&outputFormat=json&version=1.0.0&service=WFS&request=GetFeature" %>% 
-  GET() %>% 
-  content()
-
-highchart(type = "map") %>% 
-  hc_add_series(mapData = world, showInLegend = FALSE) %>% 
-  hc_add_series(data = marine, type = "mapline",  lineWidth = 2,
-                name = "Marine currents", color = 'rgba(0, 0, 80, 0.5)',
-                states = list(hover = list(color = "#BADA55")),
-                tooltip = list(pointFormat = "{point.properties.NOMBRE}")) %>%
-  hc_add_series(data = plates, type = "mapline",
-                name = "Plates", color = 'rgba(10, 10, 10, 0.5)',
-                tooltip = list(pointFormat = "{point.properties.TIPO}")) %>% 
-  hc_add_series(data = volcano, type = "mappoint",
-                name = "Volcanos", color = 'rgba(255, 0, 80, 0.5)',
-                tooltip = list(pointFormat = "{point.properties.NOMBRE}")) %>%
-  hc_mapNavigation(enabled = TRUE) %>% 
-  hc_title(text = "Testing geojson format")
-
+#+ shiny
 ##' # Shiny Integration ####
 #'
 #' Not much to say here. Highcharter is htmlwidlgets powered so it's shiny ready.
@@ -613,6 +614,7 @@ highchart(type = "map") %>%
 #' app is here https://github.com/jbkunst/shiny-apps/tree/master/highcharter.
 #' 
 
+#+ hchart
 ##' # `hchart()` function ####
 #'
 #' Right now there are some R object to plot wiht this generic function. 
