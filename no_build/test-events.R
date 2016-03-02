471cf7dbfebf2779e5d1ea6dcdd40981b36ff5aa
Joshua <jbkunst@gmail.com>
2016-03-02 18:49:22 -0300

    wup events :yay:

--- no_build/test-events.R
+++ no_build/test-events.R
@@ -2,7 +2,7 @@ rm(list = ls())
 library("highcharter")
 
 
-hc_add_event <- function(hc, typeseries = "scatter", event = "click", inputname = "inputname") {
+hc_add_event <- function(hc, typeseries = "scatter", event = "click", inputname = "hcput") {
   
   funtmp <- "function(){ alert(this.series.category)}"
   fun <- JS(sprintf(funtmp, inputname))
@@ -29,22 +29,3 @@ hc_add_event <- function(hc, typeseries = "scatter", event = "click", inputname 
 
 }
 
-fn <- "function(){
-  alert('Category: ' + this.category + ', value: ' + this.y + ', series: ' + this.series.name);
-  window.ds = this.series.data.map(function(e){ return {x: e.x, y: e.y  }  } );  
-}"
-
-hc <- hc_demo() %>%
-  hc_chart(type = "scatter") %>%  
-  hc_plotOptions(
-    series = list(
-      cursor = "pointer",
-      point = list(
-        events = list(
-          click = JS(fn)
-          )
-        )
-      )
-    ) 
-
-hc
