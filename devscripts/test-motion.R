05ed5fb1236c014ad86247ba425b5b046d8b5be8
Joshua <jbkunst@gmail.com>
2016-03-28 18:06:39 -0300

    mod example

--- devscripts/test-motion.R
+++ devscripts/test-motion.R
@@ -1,20 +1,20 @@
-hc_motion <- function(hc, ...) {
-  
-  highcharter:::.hc_opt(hc, "motion", ...)
-  
-}
-
 highchart() %>% 
-  hc_chart(type = "spline") %>% 
-  hc_yAxis(max = 7, min = 0) %>% 
-  hc_add_series(name = "JJ", data = c(2,3,4), zIndex = -10) %>% 
-  hc_add_series(name = "J",
+  hc_chart(type = "column") %>% 
+  hc_yAxis(max = 6, min = 0) %>% 
+  hc_add_series(name = "A", data = c(2,3,4), zIndex = -10) %>% 
+  hc_add_series(name = "B",
                 data = list(
                   list(sequence = c(1,2,3,4)),
                   list(sequence = c(3,2,1,3)),
-                  list(sequence = c(2,2,4,3))
+                  list(sequence = c(2,5,4,3))
                 )) %>% 
+  hc_add_series(name = "C",
+                data = list(
+                  list(sequence = c(3,2,1,3)),
+                  list(sequence = c(2,5,4,3)),
+                  list(sequence = c(1,2,3,4))
+                  )) %>% 
   hc_motion(enabled = TRUE,
             labels = 2000:2003,
-            series = 1)
+            series = c(1,2))
   
