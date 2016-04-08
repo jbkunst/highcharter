58a048e8a82870caa66dc228101cf5e026c3a0fc
Joshua <jbkunst@gmail.com>
2016-04-08 18:11:13 -0300

    custom simplegrid #67

--- devscripts/test-hc_grid.R
+++ devscripts/test-hc_grid.R
@@ -1,9 +1,48 @@
-hc_grid(
-  hc_demo(),
-  hc_demo() %>% hc_add_theme(hc_theme_ft()),
-  hc_demo() %>% hc_add_theme(hc_theme_db()),
-  hc_demo() %>% hc_add_theme(hc_theme_flat()),
-  hc_demo() %>% hc_add_theme(hc_theme_smpl()),
-  hc_demo() %>% hc_add_theme(hc_theme_economist()),
-  nrow = 2, ncol = 3, widths = 3:1/6
-) %>% htmltools::browsable()
+
+library("highcharter")
+library("dplyr", quietly = TRUE)
+rm(list = ls())
+data(economics_long, package = "ggplot2")
+
+#' # Example I
+
+grid <- hc_grid(
+  hchart(mtcars %>% dist()),
+  hchart(mtcars %>% dist() %>% hclust() %>% as.dendrogram()),
+  hchart(mtcars %>% dist() %>% hclust() %>% ape::as.phylo()),
+  ncol = 3
+)
+
+grid
+# htmltools::browsable(grid2)
+
+#' # Example II
+#' 
+
+df_with_charts2 <- economics_long %>% 
+  group_by(variable) %>% 
+  do(hc = {
+    hc_add_series_times_values(highchart(height = 150), .$date, .$value, name =  unique(.$variable)) %>%
+      hc_title(text = unique(.$variable)) %>% 
+      hc_add_theme(hc_theme_ft())
+  })
+
+
+charts2 <- df_with_charts2$hc 
+
+hc_grid(charts2, ncol = 2, nrow = 3)
+# htmltools::browsable(charts2)
+
+
+
+#' # Example III
+df_with_charts <- economics_long %>% 
+  group_by(variable) %>% 
+  do(hc = hchart(.$value, name = unique(.$variable))) 
+
+charts <- df_with_charts$hc
+
+hc_grid(charts, ncol = 5)
+# htmltools::browsable(charts) # in console
+
+
