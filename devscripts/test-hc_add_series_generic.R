0a818b8b3ff07345dcc92a71184c5254e258138f
Joshua <jbkunst@gmail.com>
2016-04-15 15:51:53 -0300

    testing generic function to add series

---
+++ devscripts/test-hc_add_series_generic.R
@@ -0,0 +1,46 @@
+hc_add_series <- function(hc, x, ...){
+  
+  clssx <- class(x)
+  
+  if(clssx %in% "list")
+    
+    hc <- hc %>% hc_add_series_list(data = x, ...)
+  
+  else if (clssx %in% c("data.frame", "tbl_df"))
+    
+    hc <- hc %>% hc_add_series(data = list.parse3(x), ...)
+  
+  else if (clssx %in% "ts")
+    
+    hc <- hc %>% hc_add_series_ts(x, ...)
+  
+  else {
+    # stop("xs of class/type ", paste(class(x), collapse = "/"),
+    #      " are not supported by hchart (yet).", call. = FALSE)
+    validate_args("add_series", eval(substitute(alist(...))))
+    hc$x$hc_opts$series <- append(hc$x$hc_opts$series, list(list(...)))
+    hc
+  }
+  
+  hc
+    
+}
+
+library("highcharter")
+
+hc <- highchart()
+
+class(AirPassengers)
+
+hc %>%  hc_add_seriesg(AirPassengers)
+
+
+library("dplyr")
+data <- data_frame(x = rnorm(100),
+                   y = x*2 + 3 + rnorm(100)*2,
+                   color = colorize_vector(x))
+
+hc %>%
+  hc_add_seriesg(data, type = "scatter")
+
+hc_add_series
