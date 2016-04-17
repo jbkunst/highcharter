02f0c1041ed89c490935b2344827251678d66167
Joshua <jbkunst@gmail.com>
2016-04-17 01:23:26 -0300

    starting waffle, closes #70

--- R/hchart_waffle.R
+++ R/hchart_waffle.R
@@ -1,28 +1,35 @@
+#' Function to create  waffle charts
+#' 
+#' @param labels A character vector
+#' @param counts A integer vector
+#' @param rows A integer to set 
+#' @param icon A string
+#' @param size Font size
+#' @importFrom purrr transpose
+#' @importFrom dplyr ungroup arrange desc group_by_
+#' @export
+hchart_waffle <- function(labels, counts, rows = NULL, icon = NULL, size = NULL){
 
+  # data(diamonds, package = "ggplot2")
+  # cnts <- count(diamonds, cut) %>%
+  #   mutate(n = n/sum(n)*500,
+  #          n = round(n)) %>%
+  #   arrange(desc(n))
+  # labels <- cnts$cut
+  # counts <- cnts$n
 
-hchart_waffle() <- function(labels, counts, multiplies = 1, rows = NULL){
-
-  data(diamonds, package = "ggplot2")
-  cnts <- count(diamonds, cut) %>%
-    mutate(n = n/sum(n)*500,
-           n = round(n)) %>% 
-    arrange(desc(n))
-  labels <- cnts$cut
-  counts <- cnts$n
-
-  dfcnts <- 
-  
   sizegrid <- n2mfrow(sum(counts))
   
   w <- sizegrid[1] 
   h <- sizegrid[2] 
   
+  
   ds <- data_frame(x = rep(1:w, h), y = rep(1:h, each = w)) %>% 
     head(sum(counts)) %>% 
-    mutate(y = -y) %>% 
+    mutate_("y" = "-y") %>% 
     mutate(gr = rep(seq_along(labels), times = counts)) %>% 
     left_join(data_frame(gr = seq_along(labels), name = as.character(labels)), by = "gr") %>% 
-    group_by(name) %>% 
+    group_by_("name") %>% 
     do(data = list.parse2(data_frame(.$x, .$y))) %>% 
     ungroup() %>% 
     left_join(data_frame(labels = as.character(labels), counts), by = c("name" = "labels")) %>% 
