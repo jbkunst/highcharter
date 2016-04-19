574e9b84218c7f93a4243bea8c42f97751e9f4ba
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-04-19 17:51:31 +0100

    Added example to demo the new feature of named attributes with scatter data

--- demo/scatter-ex.R
+++ demo/scatter-ex.R
@@ -9,3 +9,16 @@ highchart() %>%
   hc_yAxis(title = list(text = "Miles/gallon")) %>% 
   hc_tooltip(headerFormat = "<b>{series.name} cylinders</b><br>",
              pointFormat = "{point.x} (lb/1000), {point.y} (miles/gallon)")
+
+highchart() %>% 
+  hc_add_series_scatter(mtcars$wt, mtcars$mpg, car = rownames(mtcars), 
+                        hp = mtcars$hp, gear = mtcars$gear) %>% 
+  hc_chart(zoomType = "xy") %>% 
+  hc_title(text = "Motor Trend Car Road Tests") %>% 
+  hc_xAxis(title = list(text = "Weight"), minorTickInterval = "auto") %>% 
+  hc_yAxis(title = list(text = "Miles/gallon")) %>% 
+  hc_tooltip(pointFormat = "<b>{point.car}</b><br/>
+                            Weight: {point.x} lb/1000<br/>
+                            MPG: {point.y}<br/>
+                            Horsepower: {point.hp}<br/>
+                            Forward gears: {point.gear}")
