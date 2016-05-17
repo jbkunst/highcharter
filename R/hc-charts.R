4e736cb796b6d208cf2f33710be8a719c610ef4a
Joshua <jbkunst@gmail.com>
2016-05-17 17:16:17 -0300

    adding hcwaffle, hcbar, hcpie, hchist, hcboxplot, #99

--- R/hc-charts.R
+++ R/hc-charts.R
@@ -1,3 +1,48 @@
+#' Function to make a bar chart
+#' 
+#' @param x A character o factor vector.
+#' @param ... Aditional shared arguments for the data series (http://api.highcharts.com/highcharts#series).
+#' @export
+hcbar <- function(x, ...) {
+  stopifnot(is.character(x) | is.factor(x))
+  hchart(x, ...)
+}
+
+#' Function to make a pie chart
+#' @param x A character o factor vector.
+#' @param ... Aditional shared arguments for the data series (http://api.highcharts.com/highcharts#series).
+#' @export
+hcpie <- function(x, ...) {
+  stopifnot(is.character(x) | is.factor(x))
+  hchart(x, type = "pie", ...)
+}
+
+#' Function to make an histogram
+#' @param x A numeric vector.
+#' @param ... Aditional shared arguments for the data series (http://api.highcharts.com/highcharts#series).
+#' @export
+hchist <- function(x, ...) {
+  stopifnot(is.numeric(x))
+  hchart(x, ...)
+}
+
+#' Function to make a boxplot
+#' @param x A numeric vector.
+#' @param by A character vector same length of \code{x}
+#' @param ... Aditional shared arguments for the data series (http://api.highcharts.com/highcharts#series).
+#' @param outliers A boolean value to show or not the outliers
+#' @export
+hcboxplot <- function(x, by = NULL, outliers = TRUE, ...) {
+  stopifnot(is.numeric(x))
+  if (is.null(by))
+    hc_add_series_boxplot(highchart(), x, outliers = outliers, ...)
+  else
+    hc_add_series_boxplot(highchart(), x, by = by, outliers = outliers, ...)
+  
+  
+  hchart(x, ...)
+}
+
 #' Function to create  waffle charts
 #' 
 #' @param labels A character vector
@@ -8,18 +53,18 @@
 #' 
 #' @examples
 #' 
-#' hchart_waffle(c("nice", "good"), c(10, 20))
+#' hcwaffle(c("nice", "good"), c(10, 20))
 #' 
-#' hchart_waffle(c("nice", "good"), c(10, 20), size = 10)
+#' hcwaffle(c("nice", "good"), c(10, 20), size = 10)
 #' 
-#' hchart_waffle(c("nice", "good"), c(100, 200), icons = "child")
+#' hcwaffle(c("nice", "good"), c(100, 200), icons = "child")
 #' 
-#' hchart_waffle(c("car", "truck", "plane"), c(50, 20, 10), icons = c("car", "truck", "plane"))
+#' hcwaffle(c("car", "truck", "plane"), c(50, 20, 10), icons = c("car", "truck", "plane"))
 #' 
 #' @importFrom dplyr ungroup arrange desc group_by_
 #' @export
-hchart_waffle <- function(labels, counts, rows = NULL, icons = NULL, size = 4){
-
+hcwaffle <- function(labels, counts, rows = NULL, icons = NULL, size = 4){
+  
   # library(dplyr);library(purrr)
   # data(diamonds, package = "ggplot2")
   # cnts <- count(diamonds, cut) %>%
@@ -29,7 +74,7 @@ hchart_waffle <- function(labels, counts, rows = NULL, icons = NULL, size = 4){
   # labels <- cnts$cut
   # counts <- cnts$n
   # size <- 4; icon <- "diamond"
-
+  
   assertthat::assert_that(length(counts) == length(labels))
   
   hc <- highchart() 
@@ -70,7 +115,7 @@ hchart_waffle <- function(labels, counts, rows = NULL, icons = NULL, size = 4){
     ds <- ds %>% 
       left_join(dsmrk, by = "name") %>% 
       mutate(icon = fa_icon(icons))
-      
+    
   }
   
   hc <- hc %>% 
@@ -80,8 +125,6 @@ hchart_waffle <- function(labels, counts, rows = NULL, icons = NULL, size = 4){
     hc_tooltip(pointFormat = "{point.series.options.counts}") %>%
     hc_add_theme(hc_theme_null())
   
-  
   hc
-  
   
 }
