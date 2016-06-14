71280dec45f27d3b58ed4530303a5b01e3d1d0e2
Joshua <jbkunst@gmail.com>
2016-06-14 11:40:56 -0300

    updating colors on hc_theme_smpl

--- R/theme-smpl.R
+++ R/theme-smpl.R
@@ -1,7 +1,7 @@
 #' Simple Theme
 #' 
 #' Desing inspired by \url{https://github.com/hrbrmstr/hrbrmisc/blob/master/R/ggplot.r}
-#' and color by \url{http://www.materialui.co/flatuicolors}
+#' and color by \url{https://www.materialui.co/colors}
 #' 
 #' @param ... Named argument to modify the theme
 #' 
@@ -17,12 +17,12 @@ hc_theme_smpl <- function(...){
     hc_theme_merge(
       hc_theme_google(),
       hc_theme(
-        colors =  c( "#d35400",
-                     "#2980b9",
-                     "#2ecc71",
-                     "#f1c40f",
-                     "#2c3e50",
-                     "#7f8c8d" 
+        colors =  c( "#f44336",
+                     "#3F51B5",
+                     "#4CAF50",
+                     "#795548",
+                     "#607D8B",
+                     "#4CAF50" 
                      ),
         chart = list(
           style = list(
