b77ee37075d98940a5eb4f469d16db0ffefc44e9
Joshua <jbkunst@gmail.com>
2016-07-15 11:14:05 -0400

    agh, removing remote lintr, only local plz

--- devscripts/lintr_package.R
+++ devscripts/lintr_package.R
@@ -1,1 +1,7 @@
-lintr::lint_package()
+lintr::lint_package(
+  linters = lintr::with_defaults(
+    trailing_whitespace_linter = NULL,
+    camel_case_linter = NULL,
+    commented_code_linter = NULL
+    )
+)
