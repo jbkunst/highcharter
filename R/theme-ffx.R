ee999f392b0fe881134485f1ad62a78ea3430754
Joshua <jbkunst@gmail.com>
2016-09-30 16:45:17 -0300

    adding more and more themes :/

---
+++ R/theme-ffx.R
@@ -0,0 +1,75 @@
+#' Firefox
+#' 
+#' 
+#' @param ... Named argument to modify the theme
+#' 
+#' @examples 
+#' 
+#' highcharts_demo() %>% 
+#'   hc_add_theme(hc_theme_ffx())
+#' 
+#' @export
+hc_theme_ffx <- function(...){
+  
+  theme <-
+    hc_theme(
+      colors = c("#00AACC", "#FF4E00", "#B90000", "#5F9B0A", "#CD6723"),
+      chart = list(
+        backgroundColor = list(
+          linearGradient = list(0, 0, 0, 150),
+          stops = list(
+            list(0, "#CAE1F4"),
+            list(1, "#EEEEEE")
+            )
+          ),
+        style = list(
+          fontFamily = "Open Sans"
+          )
+        ),
+      title = list(
+        align = "left"
+      ),
+      subtitle = list(
+        align = "left"
+      ),
+      legend = list(
+        align = "right",
+        verticalAlign = "bottom"
+      ),
+      xAxis = list(
+        gridLineWidth = 1,
+        gridLineColor = "#F3F3F3",
+        lineColor = "#F3F3F3",
+        minorGridLineColor = "#F3F3F3",
+        tickColor = "#F3F3F3",
+        tickWidth = 1
+      ),
+      yAxis = list(
+        gridLineColor = "#F3F3F3",
+        lineColor = "#F3F3F3",
+        minorGridLineColor = "#F3F3F3",
+        tickColor = "#F3F3F3",
+        tickWidth = 1
+      )
+    )
+
+  theme <- structure(theme, class = "hc_theme")
+  
+  if (length(list(...)) > 0) {
+    theme <- hc_theme_merge(
+      theme,
+      hc_theme(...)
+    )
+  } 
+  
+  theme
+  
+}
+
+
+
+
+highcharts_demo() %>%
+  hc_add_theme(hc_theme_ffx())
+
+
---
+++ R/theme-monokai.R
@@ -0,0 +1,80 @@
+#' Monokai 
+#' 
+#' @param ... Named argument to modify the theme
+#' 
+#' @examples 
+#' 
+#' highcharts_demo() %>% 
+#'   hc_add_theme(hc_theme_monokai())
+#' 
+#' @export
+hc_theme_monokai <- function(...){
+  
+  theme <-
+    hc_theme(
+      colors = c("#F92672", "#66D9EF", "#A6E22E", "#A6E22E"),
+      chart = list(
+        backgroundColor = "#272822",
+        style = list(
+          fontFamily = "Inconsolata",
+          color = "#A2A39C"
+          )
+        ),
+      title = list(
+        style = list(
+          color = "#A2A39C"
+        ),
+        align = "left"
+      ),
+      subtitle = list(
+        style = list(
+          color = "#A2A39C"
+        ),
+        align = "left"
+      ),
+      legend = list(
+        align = "right",
+        verticalAlign = "bottom",
+        itemStyle = list(
+          fontWeight = "normal",
+          color = "#A2A39C"
+        )
+      ),
+      xAxis = list(
+        gridLineDashStyle = "Dot",
+        gridLineWidth = 1,
+        gridLineColor = "#A2A39C",
+        lineColor = "#A2A39C",
+        minorGridLineColor = "#A2A39C",
+        tickColor = "#A2A39C",
+        tickWidth = 1
+      ),
+      yAxis = list(
+        gridLineDashStyle = "Dot",
+        gridLineColor = "#A2A39C",
+        lineColor = "#A2A39C",
+        minorGridLineColor = "#A2A39C",
+        tickColor = "#A2A39C",
+        tickWidth = 1
+      )
+    )
+
+  theme <- structure(theme, class = "hc_theme")
+  
+  if (length(list(...)) > 0) {
+    theme <- hc_theme_merge(
+      theme,
+      hc_theme(...)
+    )
+  } 
+  
+  theme
+  
+}
+
+highcharts_demo() %>%
+  hc_add_theme(hc_theme_monokai())
+
+
+
+
