21b4d79339f440b607d9120666b49b117eca38fb
Joshua <jbkunst@gmail.com>
2016-11-22 17:32:01 -0300

    updating cran counts

--- devscripts/check-cran-counts.R
+++ devscripts/check-cran-counts.R
@@ -3,15 +3,18 @@ library("lubridate")
 library("cranlogs")
 library("highcharter")
 
-c("highcharter", "rbokeh", "dygraphs", "plotly", "ggvis", "metricsgraphics",
-  "rAmCharts") %>% 
+c("highcharter", "rbokeh", "dygraphs", "plotly",
+  "ggvis", "metricsgraphics", "rAmCharts") %>% 
   cran_downloads(from = "2016-01-01", to = Sys.Date()) %>% 
   tbl_df() %>% 
   mutate(date = floor_date(date, unit="week")) %>% 
   group_by(date, package) %>% 
   summarize(count = sum(count)) %>% 
+  ungroup() %>% 
+  group_by(package) %>% 
+  filter(row_number() != n()) %>%
   arrange(date) %>% 
   hchart(type = "line", x = date, y = count, group = package) %>% 
-  hc_tooltip(shared = TRUE) %>% 
+  hc_tooltip(sort = TRUE, table = TRUE) %>% 
   hc_add_theme(hc_theme_smpl())
 
