2e620100c12fd8c2569f1f383e7410023e9c0296
Joshua <jbkunst@gmail.com>
2016-12-05 16:17:46 -0300

    wip #218, almost there

--- dev/issues/issue-218.R
+++ dev/issues/issue-218.R
@@ -1,4 +1,5 @@
 library(highcharter)
+library(dplyr)
 library(htmltools)
 library(stringr)
 
@@ -7,6 +8,7 @@ hcmap <- function(map = "custom/world",
                   showInLegend = FALSE, ...) {
   
   map <- str_replace(map, "\\.js", "")
+  map <- str_replace(map, "https://code\\.highcharts\\.com/mapdata/", "")
   
   url <- "https://code.highcharts.com/mapdata/"
   
@@ -26,10 +28,16 @@ hcmap <- function(map = "custom/world",
     
   } else {
     
+    stopifnot(joinBy %in% names(data))
+    data <- mutate_(data, "value" = value)
+    
     hc <- highchart(type = "map") %>% 
       highcharter:::hc_add_series.default(
         mapData = JS(sprintf("Highcharts.maps['%s']", map)),
-        showInLegend = showInLegend, ...)
+        data = list_parse(data),
+        joinBy = joinBy,
+        showInLegend = showInLegend, ...) %>% 
+      hc_colorAxis(auxpar = NULL)
     
   }
   
@@ -40,6 +48,26 @@ hcmap <- function(map = "custom/world",
 }
 
 hcmap(nullColor = "#425668")
+
+data(GNI2014, package = "treemap")
+GNI2014 <- rename(GNI2014, "iso-a3" = iso3)
+head(GNI2014)
+
+hcmap(map = "custom/world", data = GNI2014,
+      joinBy = "iso-a3", value = "GNI")
+
+hcmap(map = "custom/world-highres3", data = GNI2014,
+      joinBy = "iso-a3", value = "GNI")
+
+
+data("USArrests", package = "datasets")
+USArrests <- mutate(USArrests, "woe-name" = rownames(USArrests))
+head(USArrests)
+
+hcmap(map = "countries/us/us-all", data = USArrests,
+      joinBy = "woe-name", value = "UrbanPop", name = "Urban Population") %>% 
+  hc_colorAxis(stops = color_stops())
+
 
 hcmap(map = "countries/us/us-all")
 hcmap(map = "countries/us/us-all-all")
