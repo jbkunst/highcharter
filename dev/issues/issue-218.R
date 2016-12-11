fc8faeb23535fc1de8d9c83758ceeb1183b7c3f3
Joshua <jbkunst@gmail.com>
2016-12-11 01:28:06 -0300

    wip #218, almost there

--- dev/issues/issue-218.R
+++ dev/issues/issue-218.R
@@ -2,11 +2,13 @@ library(highcharter)
 library(dplyr)
 library(htmltools)
 library(stringr)
+library(tibble)
+library(purrr)
 options(highcharter.debug = TRUE)
 
 hcmap <- function(map = "custom/world",
                   data = NULL, joinBy = "hc-key", value = NULL,
-                  downloadmap = FALSE, ...) {
+                  download_map_data = FALSE, ...) {
   
   url <- "https://code.highcharts.com/mapdata"
   map <- str_replace(map, "\\.js", "")
@@ -15,9 +17,9 @@ hcmap <- function(map = "custom/world",
   
   hc <- highchart(type = "map")
   
-  if(downloadmap) {
+  if(download_map_data) {
     
-    mapdata <- downloadmap(file.path(url, mapfile))
+    mapdata <- download_map_data(file.path(url, mapfile))
     
   } else {
     
@@ -57,7 +59,8 @@ hcmap <- function(map = "custom/world",
   
 }
 
-downloadmap <- function(url = "https://code.highcharts.com/mapdata/custom/world.js") {
+download_map_data <- function(url = "https://code.highcharts.com/mapdata/custom/world.js",
+                              showinfo = FALSE) {
   
   tmpfile <- tempfile(fileext = ".js")
   download.file(url, tmpfile)
@@ -66,23 +69,29 @@ downloadmap <- function(url = "https://code.highcharts.com/mapdata/custom/world.
   mapdata <- paste(mapdata, collapse = "\n")
   mapdata <- jsonlite::fromJSON(mapdata, simplifyVector = FALSE)
   
+  if(showinfo) {
+    glimpse(get_data_from_map(mapdata))
+  }
+  
   mapdata
   
 }
 
-mapdata <- downloadmap("https://code.highcharts.com/mapdata/countries/us/us-all.js")
-
-keyfrommap <- function(mapdata) {
-  
-  glimpse(map_df(mapdata$features, "properties"))
-  
+get_data_from_map <- function(mapdata) {
+  mapdata$features %>%
+    map("properties") %>%
+    map_df(function(x){ x[!map_lgl(x, is.null)] })
 }
 
-keyfrommap(downloadmap())
-keyfrommap(downloadmap("https://code.highcharts.com/mapdata/countries/us/us-all.js"))
+mapdata <- download_map_data()
+mapdata <- download_map_data("https://code.highcharts.com/mapdata/countries/us/us-all.js")
+
+
+get_data_from_map(download_map_data())
+get_data_from_map(download_map_data("https://code.highcharts.com/mapdata/countries/us/us-all.js"))
 # example 1 ---------------------------------------------------------------
 hcmap(nullColor = "#DADADA")
-hcmap(nullColor = "#DADADA", downloadmap = TRUE)
+hcmap(nullColor = "#DADADA", download_map_data = TRUE)
 
 # example 2 ---------------------------------------------------------------
 data(GNI2014, package = "treemap")
@@ -90,7 +99,7 @@ GNI2014 <- rename(GNI2014, "iso-a3" = iso3)
 head(GNI2014)
 
 hcmap(map = "custom/world", data = GNI2014, joinBy = "iso-a3", value = "GNI")
-hcmap(map = "custom/world", data = GNI2014, joinBy = "iso-a3", value = "GNI", downloadmap = TRUE)
+hcmap(map = "custom/world", data = GNI2014, joinBy = "iso-a3", value = "GNI", download_map_data = TRUE)
 hcmap(map = "custom/world-highres3", data = GNI2014, joinBy = "iso-a3", value = "GNI")
 
 # example 3 ---------------------------------------------------------------
@@ -102,7 +111,7 @@ hcmap(map = "countries/us/us-all", data = USArrests,
       joinBy = "woe-name", value = "UrbanPop", name = "Urban Population") 
 hcmap(map = "countries/us/us-all", data = USArrests,
       joinBy = "woe-name", value = "UrbanPop", name = "Urban Population",
-      downloadmap = TRUE) 
+      download_map_data = TRUE) 
 
 
 # example 4 ---------------------------------------------------------------
