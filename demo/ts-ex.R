07fa681ddab24ec3e6b726ba74169d13d0422c87
Joshua <jbkunst@gmail.com>
2016-12-26 15:18:45 -0300

    updating ts demo

--- demo/ts-ex.R
+++ demo/ts-ex.R
@@ -1,10 +1,8 @@
 highchart() %>% 
   hc_title(text = "Monthly Airline Passenger Numbers 1949-1960") %>% 
   hc_subtitle(text = "The classic Box and Jenkins airline data") %>% 
-  hc_add_series_ts(AirPassengers, name = "passengers") %>%
+  hc_xAxis(type = "datetime") %>% 
+  hc_add_series(AirPassengers, name = "passengers") %>%
   hc_tooltip(pointFormat =  '{point.y} passengers')
 
-highchart() %>% 
-  hc_title(text = "Monthly Deaths from Lung Diseases in the UK") %>% 
-  hc_add_series_ts(fdeaths, name = "Female") %>%
-  hc_add_series_ts(mdeaths, name = "Male")
+hchart(cbind(fdeaths, mdeaths), separate = FALSE)
