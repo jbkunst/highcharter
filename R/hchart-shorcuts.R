f66c8819d2b5af04900882d5a67e1a6376475377
Joshua <jbkunst@gmail.com>
2017-01-26 18:21:25 -0300

    adding hcparcords, close #167

--- R/hchart-shorcuts.R
+++ R/hchart-shorcuts.R
@@ -364,3 +364,55 @@ hctreemap <- function(tm, ...) {
   hc_add_series(highchart(), data = ds, type = "treemap", ...)
   
 }
+
+#' Shortcut for create parallel coordinates
+#' @param df A data frame object.
+#' @param ... Additional shared arguments for the data series
+#'   (\url{http://api.highcharts.com/highcharts#series}) for the
+#'   \code{hchar.data.frame} function.
+#' @examples 
+#' require(viridisLite)
+#' 
+#' n <- 15
+#' 
+#' hcparcords(head(mtcars, n), color = hex_to_rgba(magma(n), 0.5))
+#' 
+#' require(dplyr)
+#' data(iris)
+#' set.seed(123)
+#' 
+#' iris <- sample_n(iris, 60)
+#' 
+#' hcparcords(iris, color = colorize(iris$Species))
+#' @importFrom purrr dmap_if
+#' @export
+hcparcords <- function(df, ...) {
+  
+  stopifnot(is.data.frame(df))
+  
+  # df <- mtcars
+  # df <- mpg
+  
+  rescale01 <- function(x) {
+    rng <- range(x, na.rm = TRUE)
+    (x - rng[1]) / (rng[2] - rng[1])
+  }
+  
+  df <- df[map_lgl(df, is.numeric)]
+  
+  # Add row identifier
+  df <- rownames_to_column(df, ".row")
+  
+  df <- dmap_if(df, is.numeric, rescale01)
+  
+  df <- tidyr::gather_(df, "var", "val", setdiff(names(df), ".row"))
+  
+  hchart(df, "line", hcaes_(x = "var", y = "val", group = ".row"), ...) %>% 
+    hc_plotOptions(series = list(showInLegend = FALSE)) %>% 
+    hc_yAxis(min = 0, max = 1) %>% 
+    hc_tooltip(sort = TRUE, table = TRUE, valueDecimals = 2) 
+  
+}
+
+
+
