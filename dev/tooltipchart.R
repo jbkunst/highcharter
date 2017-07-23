8bc3d5b2ed6e0b4954bbcf770b3f2a46534b8271
jbkunst <jbkunst@gmail.com>
2017-07-23 13:52:06 -0400

    yayaya almost there

--- dev/tooltipchart.R
+++ dev/tooltipchart.R
@@ -1,121 +1,155 @@
+rm(list  = ls())
 library(highcharter)
 library(gapminder)
 library(dplyr)
 library(purrr)
 library(tidyr)
 library(rlang)
+library(jsonlite)
+library(whisker)
+library(stringr)
+data(gapminder, package = "gapminder")
+glimpse(gapminder)
+options(highcharter.theme = hc_theme_smpl())
+
+# data --------------------------------------------------------------------
 data(gapminder, package = "gapminder")
 glimpse(gapminder)
 
-
-# data --------------------------------------------------------------------
 gp <- gapminder %>% 
   arrange(desc(year)) %>% 
   distinct(country, .keep_all = TRUE)
 gp
 
+hc <- hchart(gp, "point", hcaes(lifeExp, gdpPercap, size = pop, group = continent))
 
-gpmini <- ""
+hc %>% 
+  hc_yAxis(type = "logarithmic")
 
-data <- gapminder
+gp2 <- gapminder %>% 
+  group_by(country) %>% 
+  do(minidata = .$lifeExp)
+gp2
 
-mppng <- hcaes(x = year, y = lifeExp)
 
-
-gpmini <- gapminder %>% 
-  arrange(year) %>%
+gp2 <- gapminder %>% 
   nest(-country) %>% 
-  mutate(data = map(data, mutate_mapping, hcaes(x = year, y = lifeExp), drop = TRUE),
+  mutate(data = map(data, mutate_mapping, hcaes(x = lifeExp, y = gdpPercap), drop = TRUE),
          data = map(data, list_parse)) %>% 
-  rename(tooltipdata = data)
+  rename(minidata = data)
+gp2
 
-gptot <- left_join(gp, gpmini)
-
-gptot
-
-hchart(gp, "point", hcaes(lifeExp, gdpPercap, size = pop, group = continent)) %>% 
-  hc_yAxis(type = "logarithmic") %>% 
-
-hc
-
-point_formatter_minichart <- 
-  function(
-    hc_opts = list(
-      series = list(list(data = JS("thiz.tooltipdata"))),
-      legend = list(enabled = FALSE)
-      ),
-    width = 250,
-    height = 150
-    ) {
-  
-  
-  id <- highcharter:::random_id()
-  
-  hcopts <- toJSON(hc_opts, pretty = TRUE, auto_unbox = TRUE, force = TRUE, null = "null", na = "null")
-  hcopts <- as.character(hcopts)
-  
-  jss <- sprintf("function() {
-  var thiz = this;
-  setTimeout(function() {
-
-    $('tooltipchart-%s').highcharts({%s})
-
-  }, 0)
-
-  return '<div id=\"tooltipchart-%s\" style=\"width: %s; height: %s;\"></div>';
-
-  }", id, hcopts, id, width, height)
-  
-  jss
-  
-}
-
-library(highcharter)
-point_formatter_minichart()
+gptot <- left_join(gp, gp2)
 
 
-
-
-minichart <- "function(){
-var thiz = this;
-console.log(thiz);
-setTimeout(function() {$('#minichart').highcharts({});}, 0);
-return '<div id=\"minichart\" style=\"width: 250px; height: 150px;\"></div>';
-}                        
-"
-
-hc <- hc %>% 
-  hc_tooltip(
-    useHTML = TRUE,
-    positioner = JS("function () { return { x: this.chart.plotLeft + 0, y: 0 }; }"),
-    headerFormat = "{point.country}",
-    pointFormatter = JS(minichart)
-  )
-hc
-
-gp3 <- gapminder %>% 
-  select(country, x = lifeExp, y = gdpPercap, z = pop) %>% 
-  nest(-country) %>% 
-  rename(sequence = data) %>% 
-  mutate(sequence = map(sequence, list_parse))
-
-gp <- left_join(gp, gp3)
-
-hc <- hchart(gp, "point", hcaes(lifeExp, gdpPercap, size = pop, group = continent)) %>% 
+hc <- hchart(gptot, "point", hcaes(lifeExp, gdpPercap, name = country, size = pop, group = continent)) %>% 
   hc_yAxis(type = "logarithmic")
 
 hc
 
-hc <- hc %>% 
-  hc_motion(enabled = TRUE, series = 0:4, labels = sort(unique(gapminder$year)),
-            loop = FALSE, autoPlay = TRUE, 
-            updateInterval = 500, magnet = list(step =  1)) %>% 
-  hc_xAxis(min = 20, max = 90) %>% 
-  hc_yAxis(type = "logarithmic", min = 100, max = 100000) %>% 
+minichart <- "function(){
+  var point = this;
+  console.log(point);
+  setTimeout(function() {
+    $(\"#minichart\").highcharts({
+      series: [{
+      animation: false,
+      type: \"scatter\",
+      name: point.name,
+      data: point.minidata
+      }]
+    });
+  }, 0);
+  return '<div id=\"minichart\" style=\"width: 250px; height: 150px;\"></div>';
+}                        
+"
+
+hc %>% 
   hc_tooltip(
     useHTML = TRUE,
     positioner = JS("function () { return { x: this.chart.plotLeft + 0, y: 0 }; }"),
-    headerFormat = "{point.country}",
+    pointFormatter = JS(minichart)
+      )
+
+point_formatter_minichart <- function(
+  accesor = "minidata",
+  hc_opts = list(
+      series = list(list(animation = FALSE, type = "scatter", name = "point.name"))
+      ),
+  width = 250,
+  height = 150
+) {
+
+  id <- highcharter:::random_id()
+
+  hc_opts[["series"]][[1]][["data"]] <- sprintf("point.%s", accesor)
+
+  hcopts <- toJSON(hc_opts, pretty = TRUE, auto_unbox = TRUE, force = TRUE, null = "null", na = "null")
+  hcopts <- as.character(hcopts)
+  cat(hcopts)
+
+
+  ts <- stringr::str_extract_all(hcopts, "\"point\\.\\w+\"") %>%  unlist()
+  for(t in ts) hcopts <- str_replace(hcopts, t, str_replace_all(t, "\"", ""))
+
+  
+  ts <- stringr::str_extract_all(hcopts, "\"\\w+\":") %>%  unlist()
+  for(t in ts) {
+    t2 <- str_replace_all(t, "\"", "")
+    # t2 <- str_replace(t2, ":", "")
+    hcopts <- str_replace(hcopts, t, t2)
+  }
+  
+  
+  jss <- "function() {
+  var point = this;
+  console.log(point);
+  setTimeout(function() {
+
+    $(\"#minichart\").highcharts(hcopts);
+
+  }, 0);
+
+  return '<div id=\"minichart\" style=\"width: {{w}}px; height: {{h}}px;\"></div>';
+
+  }"
+
+  cat(jss)
+
+  jsss <- whisker.render(jss, list(dataaccesor = accesor, mc = id, w = width, h = height))
+  cat(jsss)
+
+  jsss <- stringr::str_replace(jsss, "hcopts", hcopts)
+  cat(jsss)
+
+  JS(jsss)
+
+}
+
+pfmc <-  point_formatter_minichart()
+
+cat(minichart)
+cat(pfmc)
+
+hc %>% 
+  hc_tooltip(
+    useHTML = TRUE,
+    positioner = JS("function () { return { x: this.chart.plotLeft + 0, y: 0 }; }"),
+    pointFormatter = pfmc
+  )
+
+hc %>% 
+  hc_tooltip(
+    useHTML = TRUE,
+    positioner = JS("function () { return { x: this.chart.plotLeft + 0, y: 0 }; }"),
     pointFormatter = JS(minichart)
   )
-hc
+
+x <- c(as.character(minichart), as.character(pfmc))
+
+y <- x %>% 
+  str_replace_all("\\s+", "") 
+
+y
+y[1]==y[2]
+
