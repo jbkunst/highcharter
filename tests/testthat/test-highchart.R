ca455c66015b32dd789d9d91f2b912f1fd0d9e8f
Pachamaltese <mvargas@dcc.uchile.cl>
2019-11-08 12:14:18 -0300

    hchart tests

--- tests/testthat/test-highchart.R
+++ tests/testthat/test-highchart.R
@@ -1,4 +1,4 @@
-context("Testing the highchart constructor")
+# highchart ----
 
 test_that("highchart object is a from highchart and htmlwidget class", {
   hc <- highchart()
@@ -6,12 +6,49 @@ test_that("highchart object is a from highchart and htmlwidget class", {
   expect_true(all(class(hc) %in% c("highchart", "htmlwidget")))
 })
 
-test_that("highchartOutput  return a function", {
+# highchartOutput ----
+
+test_that("highchartOutput returns a function", {
   expect_true(
     all(class(highchartOutput("outputid")) %in% c("shiny.tag.list", "list"))
   )
 })
 
-test_that("renderHighchart return a function", {
+# hchart ----
+
+test_that("renderHighchart returns a function", {
   expect_true(is.function(renderHighchart(highchart())))
+})
+
+test_that("hchart returns a valid plot after valid data input", {
+  data(mpg, package = "ggplot2")
+  h <- hchart(mpg, "scatter", hcaes(x = displ, y = hwy, group = class))
+  expect_true(all(class(h) %in% c("highchart","htmlwidget")))
+})
+
+test_that("hchart returns a valid scatter plot after valid data input", {
+  data(mpg, package = "ggplot2")
+  h <- hchart(mpg, "scatter", hcaes(x = displ, y = hwy, group = class))
+})
+
+test_that("hchart returns a valid area plot after valid data input", {
+  data(mpg, package = "ggplot2")
+  x <- mpg$hwy
+  h <- hchart(density(x), type = "area", color = "#B71C1C", name = "hwy")
+})
+
+test_that("hchart fails after non-existing data", {
+  # mgp = mpg mispelled
+  expect_error(hchart(mgp, "scatter", hcaes(x = displ, y = hwy, group = class)))
+})
+
+test_that("hchart fails after mispelled hcaes", {
+  data(mpg, package = "ggplot2")
+  # dipsl = displ mispelled
+  expect_error(hchart(mpg, "scatter", hcaes(x = dipsl, y = hwy, group = class)))
+})
+
+test_that("hchart fails after mispelled chart type", {
+  # sactter = scatter mispelled
+  expect_error(hchart(mgp, "sactter", hcaes(x = displ, y = hwy, group = class)))
 })
