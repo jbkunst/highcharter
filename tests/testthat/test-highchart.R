d75397b5517215fbb5d78053ff8189345b99e22a
Pachamaltese <mvargas@dcc.uchile.cl>
2019-11-08 12:56:12 -0300

    test ts

--- tests/testthat/test-highchart.R
+++ tests/testthat/test-highchart.R
@@ -20,7 +20,7 @@ test_that("renderHighchart returns a function", {
   expect_true(is.function(renderHighchart(highchart())))
 })
 
-test_that("hchart returns a valid plot after valid data input", {
+test_that("hchart has a valid class after valid data input", {
   data(mpg, package = "ggplot2")
   h <- hchart(mpg, "scatter", hcaes(x = displ, y = hwy, group = class))
   expect_true(all(class(h) %in% c("highchart","htmlwidget")))
@@ -29,12 +29,38 @@ test_that("hchart returns a valid plot after valid data input", {
 test_that("hchart returns a valid scatter plot after valid data input", {
   data(mpg, package = "ggplot2")
   h <- hchart(mpg, "scatter", hcaes(x = displ, y = hwy, group = class))
+  expect_true(all(class(h) %in% c("highchart","htmlwidget")))
 })
 
 test_that("hchart returns a valid area plot after valid data input", {
   data(mpg, package = "ggplot2")
   x <- mpg$hwy
   h <- hchart(density(x), type = "area", color = "#B71C1C", name = "hwy")
+  expect_true(all(class(h) %in% c("highchart","htmlwidget")))
+})
+
+test_that("hchart returns a valid column plot after valid data input", {
+  data(mpg, package = "ggplot2")
+  x <- mpg$hwy
+  h <- hchart(x, type = "column", color = "#B71C1C", name = "hwy")
+  expect_true(all(class(h) %in% c("highchart","htmlwidget")))
+})
+
+test_that("hchart returns a valid time series plot after valid data input", {
+  h <- hchart(LakeHuron)
+  expect_true(all(class(h) %in% c("highchart","htmlwidget")))
+})
+
+test_that("hchart returns a valid time series decomposition plot after valid data input", {
+  x <- stl(log(AirPassengers), "per")
+  h <- hchart(x)
+  expect_true(all(class(h) %in% c("highchart","htmlwidget")))
+})
+
+test_that("hchart returns a valid time series forecast plot after valid data input", {
+  x <- forecast::forecast(forecast::ets(USAccDeaths), h = 48, level = 95)
+  h <- hchart(x)
+  expect_true(all(class(h) %in% c("highchart","htmlwidget")))
 })
 
 test_that("hchart fails after non-existing data", {
