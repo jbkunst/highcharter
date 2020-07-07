ffb69e522a762fddb38a9681c25fd4b246388247
Joshua <jbkunst@gmail.com>
2020-07-06 21:22:15 -0400

    adding lollipop chart

---
+++ dev/examples-charts/lollipop.R
@@ -0,0 +1,6 @@
+mtcars <- mtcars[order(mtcars$hp , decreasing = TRUE),]
+mtcars$name <- rownames(mtcars)
+
+hchart(mtcars, "lollipop", hcaes(name = name, low = hp ), name = "Horse Power") %>% 
+  hc_xAxis(type = "category") %>% 
+  hc_yAxis(labels = list(format = "{value} HP"))
