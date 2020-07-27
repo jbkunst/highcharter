c2b2d63843320a6e99fbcc33bb72251335f79822
Joshua <jbkunst@gmail.com>
2020-07-27 13:39:19 -0400

    changes in docs

--- pkgdown/99-pkgdown-run-all.R
+++ pkgdown/99-pkgdown-run-all.R
@@ -1,4 +1,4 @@
-pkgdown::build_site()
+# pkgdown::build_site()
 source("pkgdown/01-pkgdown-buid-home.R")
 source("pkgdown/02-pkgdown-add-to-yalm-reference.R")
 source("pkgdown/03-pkgdown-add-to-yalm-articles.R")
