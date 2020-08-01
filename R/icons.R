69ba8b4ff7eec94b94f1cf89f9593caeca1f360d
Joshua <jbkunst@gmail.com>
2020-08-01 00:48:19 -0400

    rm examples icons

--- R/icons.R
+++ R/icons.R
@@ -1,28 +1,5 @@
 #' Helpers functions to get FontAwesome icons code
 #' @param hc A `highchart` `htmlwidget` object.
-#' @examples
-#'
-#' dcars <- data.frame(x = runif(10), y = runif(10))
-#' dtrck <- data.frame(x = rexp(10), y = rexp(10))
-#'
-#' highchart() %>%
-#'   hc_chart(zoomType = "xy") %>%
-#'   hc_tooltip(
-#'     useHTML = TRUE,
-#'     pointFormat = paste0(
-#'       "<span style=\"color:{series.color};\">{series.options.icon}</span>",
-#'       "{series.name}: <b>[{point.x}, {point.y}]</b><br/>"
-#'     )
-#'   ) %>%
-#'   hc_add_series(dcars, "scatter",
-#'     marker = list(symbol = fa_icon_mark("car")),
-#'     icon = fa_icon("car"), name = "car"
-#'   ) %>%
-#'   hc_add_series(dtrck, "scatter",
-#'     marker = list(symbol = fa_icon_mark("plane")),
-#'     icon = fa_icon("plane"), name = "plane"
-#'   ) %>%
-#'   hc_add_dependency_fa()
 #' @export
 hc_add_dependency_fa <- function(hc) {
   .Deprecated(msg = "Deprecated function. Use the fontawesome package. Example in the docs website.")
@@ -40,8 +17,6 @@ hc_add_dependency_fa <- function(hc) {
 
 #' @rdname hc_add_dependency_fa
 #' @param iconname The icon's name
-#' @examples
-#' fa_icon("car")
 #' @export
 fa_icon <- function(iconname = "circle") {
   .Deprecated(msg = "Deprecated function. Use the fontawesome package. Example in the docs website.")
@@ -49,9 +24,6 @@ fa_icon <- function(iconname = "circle") {
 }
 
 #' @rdname hc_add_dependency_fa
-#' @examples
-#' fa_icon_mark("car")
-#' fa_icon_mark(iconname = c("car", "plane", "car"))
 #' @export
 fa_icon_mark <- function(iconname = "circle") {
   
